15f61a072207c3944201d683f5a91f54feb46532
ò_ó <donmahallem@users.noreply.github.com>
2025-01-31 11:01:12 +0100

    ci(github): enable unit testing in ci (#23)
    * ci(github): add testing
    
    * ci(github): add testing

--- src/app/routes/hass/hass-home.component.spec.ts
+++ src/app/routes/hass/hass-home.component.spec.ts
@@ -1,29 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { HassHomeComponent } from './hass-home.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [HassHomeComponent],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HassHomeComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'herpes_home' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HassHomeComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('herpes_home');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, herpes_home');
   });
 });
